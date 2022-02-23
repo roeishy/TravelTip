d6a0893252a2831516ded84d22511d6eb0555037
roeishy <roeishy3@gmail.com>
2022-02-23 15:41:39 +0200

    controller: render map, locations, modal location-service: create and save locations

---
+++ js/services/location-service.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const STORAGE_KEY = 'SavedLocations'
+var gSavedLocations = [];
+
+function createLocation(pos, name) {
+    var location = {
+        id: makeId(),
+        name: name,
+        pos,
+        createdAt: Date.now(),
+        updatedAt: Date.now()
+    }
+    gSavedLocations.push(location);
+    saveToStorage(STORAGE_KEY, gSavedLocations)
+}
+
+function _saveLocsToStorage() {
+    saveToStorage(STORAGE_KEY, gSavedPos)
+}
+
+function getSavedPos() {
+    gSavedLocations = loadFromStorage(STORAGE_KEY);
+    return gSavedLocations;
+}
